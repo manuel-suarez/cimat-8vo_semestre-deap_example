046e4f8097d68487ec60bf133b4479672de9ef1b
Arturo Suárez <manuel.suarez@cimat.mx>
2025-01-14 00:03:26 -0600

    Delete unused modules

--- process.py
+++
@@ -1,2 +0,0 @@
-def unknown(x):
-    return 1.3 * x + 1.9 * x**2 - 4.2 * x**3 + 5.0
--- sampling.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from numpy import random
-from process import unknown
-
-
-def sample(inputs):
-    return np.array([unknown(inp) + random.normal(5.0) for inp in inputs])
